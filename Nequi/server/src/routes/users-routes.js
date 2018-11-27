5cc7fffeb1b900b4b64e9afa2542ee4cea089a34
Johann Sebastian Ortiz Salazar <sebastiansalazaro@hotmail.com>
2018-11-26 21:37:11 -0500

    Express Rest API and Front End with Admin On Rest Framework

---
+++ Nequi/server/src/routes/users-routes.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const router = express.Router();
+const connection = require('../database');
+
+
+router.get('/', (req, res, next) => {
+    connection.query('SELECT * FROM user', (err, rows, fields) => {
+        if (!err) {
+            res.setHeader("X-Total-Count", rows.length);
+            res.json(rows);
+        } else {
+            console.log(err);
+        }
+    });
+});
+
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+    connection.query('SELECT * FROM user WHERE id = ?', [id], (err, rows, field) => {
+        if (!err) {
+            res.json(rows[0]);
+        } else {
+            console.log(err);
+        }
+    });
+});
+
+router.post('/', (req, res) => {
+    const user = req.body;
+    connection.query('INSERT INTO user SET ?', [user], (err, rows, field) => {
+        if (!err) {
+            res.json({
+                status: 'User Created'
+            });
+        } else {
+            console.log(err);
+        }
+    });
+});
+
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    connection.query('DELETE FROM user WHERE id = ?', [id], (err, rows, field) => {
+        if (!err) {
+            res.json({
+                status: 'User Deleted'
+            });
+        } else {
+            console.log(err);
+        }
+    });
+});
+
+router.put('/:id', (req, res) => {
+    const { id } = req.params;
+    const user = req.body;
+    connection.query('UPDATE user SET ? WHERE id = ?', [user, id], (err, rows, field) => {
+        if (!err) {
+            res.json({
+                status: 'User Updated'
+            });
+        } else {
+            console.log(err);
+        }
+    });
+});
+
+module.exports = router; 
